de05760d6e2f08943d943e3203c1e7b6a8d2c2d3
riddhima-gkmit <riddhima@gkmit.co>
2025-12-07 15:22:41 +0530

    feat: add basic calculator code

---
+++ main.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    x = int(input("Enter first number: "))
+    y = int(input("Enter second number: "))
+    print("Result:", add(x, y))
